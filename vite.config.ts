04ba2b0a8a21954d4528975152a4845da926b9de
sumitgoyal00 <sumitgoy00@gmail.com>
2026-09-16 00:36:08 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return { 
-    base: '/creative-thinker-v0.5/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
